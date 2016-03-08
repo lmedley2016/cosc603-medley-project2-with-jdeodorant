427161ba86f926499b3f4111f28e93b2a4e07891
lmedley2016 <lmedle1@students.towson.edu>
2016-03-07 23:16:40 -0500

    Commit before starting JDeodorant.  This version of the code had the refactoring prior to using JDeodorant.

---
+++ Monopoly/src/edu/towson/cis/cosc603/project2/monopoly/Cell.java
@@ -0,0 +1,92 @@
+package edu.towson.cis.cosc603.project2.monopoly;
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Class Cell.
+ */
+public abstract class Cell implements IOwnable {
+	
+	/** The name. */
+	private String name;
+	
+	/** The theOwner. */
+	protected Player theOwner;
+
+	/** The available. */
+	private boolean available = true;
+
+	/**
+	 * Gets the name.
+	 *
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/* (non-Javadoc)
+	 * @see edu.towson.cis.cosc603.project2.monopoly.IOwnable#getTheOwner()
+	 */
+	@Override
+	public Player getTheOwner() {
+		return theOwner;
+	}
+	
+	/**
+	 * Gets the price.
+	 *
+	 * @return the price
+	 */
+	public int getPrice() {
+		return 0;
+	}
+
+	/**
+	 * Play action.
+	 * @param msg TODO
+	 * @return TODO
+	 */
+	public abstract Boolean playAction(String msg);
+
+	/**
+	 * Sets the name.
+	 *
+	 * @param name the new name
+	 */
+	void setName(String name) {
+		this.name = name;
+	}
+
+	/* (non-Javadoc)
+	 * @see edu.towson.cis.cosc603.project2.monopoly.IOwnable#setTheOwner(edu.towson.cis.cosc603.project2.monopoly.Player)
+	 */
+	@Override
+	public void setTheOwner(Player theOwner) {
+		this.theOwner = theOwner;
+	}
+    
+    /* (non-Javadoc)
+     * @see java.lang.Object#toString()
+     */
+    public String toString() {
+        return name;
+    }
+
+	/**
+	 * Checks if is available.
+	 *
+	 * @return true, if is available
+	 */
+	public boolean isAvailable() {
+		return available;
+	}
+
+	/**
+	 * Sets the available.
+	 *
+	 * @param available the new available
+	 */
+	public void setAvailable(boolean available) {
+		this.available = available;
+	}
+}
---
+++ Monopoly/src/edu/towson/cis/cosc603/project2/monopoly/IOwnable.java
@@ -0,0 +1,23 @@
+package edu.towson.cis.cosc603.project2.monopoly;
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Interface IOwnable.
+ */
+public interface IOwnable {
+
+	/**
+	 * Gets the theOwner.
+	 *
+	 * @return the theOwner
+	 */
+	public abstract Player getTheOwner();
+
+	/**
+	 * Sets the theOwner.
+	 *
+	 * @param theOwner the new theOwner
+	 */
+	public abstract void setTheOwner(Player theOwner);
+
+}
